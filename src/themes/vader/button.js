b4563cd415a99ada8ee0555e92c78d514123f344
Augustin Berisa <burim.berisa@protonmail.com>
2021-06-16 01:25:39 +0200

    Styling, buttons and icons

--- src/themes/vader/button.js
+++ src/themes/vader/button.js
@@ -10,47 +10,39 @@ export default {
 	variants: {
 		solid: () => ({
 			color: '#000',
-			background: 'linear-gradient(90deg, rgb(255, 157, 219) 0%, rgb(38, 164, 254) 100%)',
+			backgroundImage: 'linear-gradient(90deg, rgb(255, 157, 219) 0%, rgb(38, 164, 254) 100%)',
 			_hover: {
-				backgroundPosition: 'right center',
 				backgroundImage: 'linear-gradient(90deg, rgb(254, 201, 254) 0%, rgb(56, 233, 253) 100%)',
 				_disabled: {
-					backgroundImage: 'linear-gradient(90deg,#f472b6  0%, #3b82f6 100%)',
-					background: 'transparent',
+					background: 'linear-gradient(90deg, rgb(128, 79, 110) 0%, rgb(4, 54, 89) 100%)',
 				},
 			},
 			_active: {
-				backgroundSize: '150% auto',
-				backgroundPosition: 'right center',
-				backgroundImage: 'linear-gradient(90deg, rgba(244,139,194,1) 0%, rgba(83,145,245,1) 100%)',
+				backgroundImage: 'linear-gradient(90deg, rgb(254, 201, 254) 0%, rgb(56, 233, 253) 100%)',
+				opacity: '0.7',
 			},
 			_disabled: {
-				color: '#fff',
+				color: '#000',
+				background: 'linear-gradient(90deg, rgb(128, 79, 110) 0%, rgb(4, 54, 89) 100%)',
 			},
 		}),
 		solidRadial: () => ({
 			color: '#000',
-			transition: '1s',
-			backgroundSize: '150% auto',
 			borderRadius: '9rem',
-			backgroundImage: 'linear-gradient(90deg,#f472b6  0%, #3b82f6 100%)',
+			backgroundImage: 'linear-gradient(90deg, rgb(255, 157, 219) 0%, rgb(38, 164, 254) 100%)',
 			_hover: {
-				backgroundSize: '150% auto',
-				backgroundPosition: 'right center',
-				backgroundImage: 'linear-gradient(90deg,#f472b6  0%, #3b82f6 100%)',
+				backgroundImage: 'linear-gradient(90deg, rgb(254, 201, 254) 0%, rgb(56, 233, 253) 100%)',
 				_disabled: {
-					backgroundSize: '150% auto',
-					backgroundImage: 'linear-gradient(90deg,#f472b6  0%, #3b82f6 100%)',
-					background: 'transparent',
+					backgroundImage: 'linear-gradient(90deg, rgb(128, 79, 110) 0%, rgb(4, 54, 89) 100%)',
 				},
 			},
 			_active: {
-				backgroundSize: '150% auto',
-				backgroundPosition: 'right center',
-				backgroundImage: 'linear-gradient(90deg, rgba(244,139,194,1) 0%, rgba(83,145,245,1) 100%)',
+				backgroundImage: 'linear-gradient(90deg, rgb(254, 201, 254) 0%, rgb(56, 233, 253) 100%)',
+				opacity: '0.7',
 			},
 			_disabled: {
-				color: '#fff',
+				color: '#000',
+				background: 'linear-gradient(90deg, rgb(128, 79, 110) 0%, rgb(4, 54, 89) 100%)',
 			},
 		}),
 		outline: () => ({
@@ -78,6 +70,14 @@ export default {
 			_active: {
 				color: '#fff',
 				opacity: '0.6',
+			},
+		}),
+		ghost: () => ({
+			_hover: {
+				background: 'transparent',
+			},
+			_active: {
+				background: 'transparent',
 			},
 		}),
 		linkAccent: () => ({
--- src/themes/vader/index.js
+++ src/themes/vader/index.js
@@ -79,11 +79,11 @@ const overrides = {
 	layerStyles: {
 		colorful: {
 			borderRadius: '12px',
-			background: 'linear-gradient(90deg,#ff9ddb 0%, #26a4fe 100%)',
+			background: 'linear-gradient(90deg,rgb(100, 71, 101) 0%,rgb(33, 74, 112) 100%)',
 			maxHeight: '660px',
 		},
 		inputLike: {
-			background: 'linear-gradient(90deg,#a25b80 0%, #406299 100%)',
+			background: 'linear-gradient(90deg,#845a81 0%,#515a85 100%)',
 			borderRadius: '0.8rem',
 			padding: '0.6rem 1rem',
 		},
