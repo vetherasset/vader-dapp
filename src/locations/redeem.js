faf4c3c0b99bc97e1302506de3bada65928c81e5
hankxstudio <hankx@momane.com>
2021-06-16 23:08:24 +0800

    done redeem page ui

--- src/locations/redeem.js
+++ src/locations/redeem.js
@@ -1,17 +1,49 @@
 import React, { useState, useEffect } from 'react'
-import { Box, Flex, Text, Input, Button } from '@chakra-ui/react'
+import {
+	Box,
+	Flex,
+	Text,
+	Button,
+	NumberInput,
+	NumberInputField,
+	Image,
+	List,
+	ListItem,
+	ListIcon,
+} from '@chakra-ui/react'
+import vaderIcon from '../static/icons/vader.svg'
+
 import {
 	FormControl,
 	FormLabel,
 	FormErrorMessage,
-	FormHelperText,
 	Select,
 } from '@chakra-ui/react'
 import defaults from '../common/defaults'
 import axios from 'axios'
+import { TriangleDownIcon } from '@chakra-ui/icons'
+
+const HiddenList = {
+	visibility: 'hidden',
+	opacity: 0,
+	display: 'none',
+}
+
+const ShowList = {
+	position: 'absolute',
+	transition: 'all 0.5s ease',
+	marginTop: '1rem',
+	left: 0,
+}
+
+const ToggleList = {
+	visibility: 'visible',
+	opacity: 1,
+	display: 'block',
+}
+
 
 export const Redeem = () => {
-
 	const fakeTokens = [{
 		name: 'Vader',
 		value: 'vader',
@@ -24,13 +56,15 @@ export const Redeem = () => {
 	},
 	]
 	const [tokens, setTokens] = useState(fakeTokens)
-	const [tokenToBurn, setTokenToBurn] = useState('')
+	const [tokenToBurn, setTokenToBurn] = useState('Vader')
 	const [burnAmount, setBurnAmount] = useState(0)
-	const [usdvToGet, setUsdvToGet] = useState(0)
+	const [tokenToGet, setTokenToGet] = useState('USDV')
+	const [tokenAmountToGet, setTokenAmountToGet] = useState(10000)
+	const [showTokenList, setShowTokenList] = useState(false)
 
 
 	const burnToken = () => {
-	  console.log(tokenToBurn)
+		console.log(tokenToBurn)
 		console.log(burnAmount)
 	}
 
@@ -38,49 +72,77 @@ export const Redeem = () => {
 		<Box
 			maxWidth={defaults.layout.container.sm.width}
 			m='0 auto'
+			pt='5rem'
 		>
-			<Flex color='white' justifyContent='center'>
-				<Box bg='white' color='black'
-					borderWidth='1px' borderRadius='lg' p='2rem' width={{ base: '100%', md: '80%', lg: '70%' }}>
-					<Text align='center' fontSize='18px' fontWeight='bolder'>
+			<Flex
+				justifyContent='center'
+				layerStyle='colorful'
+				maxW='49ch'
+				m='0 auto'
+				p='1.8rem'
+			>
+				<Box>
+					<Text align='center' fontSize='1.5rem' fontWeight='bolder'>
             Asset redemption
 					</Text>
-					<Text align='center' fontSize='14px' display='block' mb='2rem'>
+					<Text align='center' fontSize='1.2rem' display='block' mb='2rem'>
             Redeem assets by burning your tokens
 					</Text>
 					<FormControl id='assetToBurn'>
-						<FormHelperText>some helper</FormHelperText>
-						<FormLabel> Asset amount to burn</FormLabel>
+						<FormLabel fontSize='1.2rem' fontWeight='bolder'> Asset amount to burn</FormLabel>
 						<FormErrorMessage />
-						<Box border='1px' d='flex' borderRadius='8px'>
-							<Select placeholder='Select Token' w='40%' style={{ textTransform: 'uppercase' }}
-								_hover={{ border: 'none' }} _focus={{ border: 'none' }}
-								onChange={(e) => {
-									setTokenToBurn(e.target.value)
-								}}>
-								{tokens.map(t => <option value={t.name} key={t.name}>{t.name}</option>,
-								)}
-							</Select>
-							<Input w='60%' border={{ base: 'none' }} type='number' value={burnAmount}
-								textAlign='right' _hover={{ border: 'none' }} _focus={{ border: 'none' }}
-								onInput={(e) => {
-									setBurnAmount(Number(e.target.value))
-								}} />
-						</Box>
-						<Box textAlign='center' marginY='15px'>...mint</Box>
+						<Flex layerStyle='inputLike'>
+							<Box flex='1' pr='0.5rem'>
+								<NumberInput>
+									<NumberInputField placeholder='0.0' border='none' fontSize="1.5rem"/>
+								</NumberInput>
+							</Box>
+							<Box position='relative' cursor='pointer' onClick={() => setShowTokenList(!showTokenList)}>
+								<Box d='flex' alignItems='center'>
+									<Image
+										width='42px'
+										mr='10px'
+										src={vaderIcon}
+									/>
+									<Box as='h3' m='0' fontSize='xl' fontWeight='bold' textTransform='capitalize'>{tokenToBurn}</Box>
+									<TriangleDownIcon ml={1} />
+								</Box>
+								<Box {...(showTokenList ? ShowList : HiddenList)} layerStyle="colorful" padding="1rem" mt=".7rem">
+									<List {...ToggleList}>
+										{tokens.map(token =>
+											<ListItem key={token.name} mb="0.5rem" d="flex" alignItems="center" onClick={()=> setTokenToBurn(token.value)}>
+												<Image
+													width='42px'
+													mr='10px'
+													src={vaderIcon}
+												/>
+												{token.name}
+											</ListItem>,
+										)}
+									</List>
+								</Box>
+							</Box>
+						</Flex>
+						<Box textAlign='center' marginY='15px'>Mint:</Box>
 						<Box d='flex' justifyContent='center' alignItems='center'>
-							<Box as='span' marginX='1rem' _before={{
-								content: '" "',
-								width: '1.5rem',
-								height: '1.5rem',
-								backgroundColor: 'pink',
-								borderRadius: '.75rem',
-								display: 'block',
-							}} />
-							<Text fontSize='2rem'>{usdvToGet} USDV</Text>
+							<Box fontSize='1.5rem' fontWeight='bolder' mr="1rem">{tokenAmountToGet}</Box>
+							<Image
+								width='42px'
+								mr='10px'
+								src={vaderIcon}
+							/>
+							<Box fontSize='1.5rem' fontWeight='bolder'>{tokenToGet}</Box>
 						</Box>
 						<Box d='flex' justifyContent='center' marginY='1.5rem'>
-							<Button w='100%' fontSize="2rem" padding="1rem" height="4rem" onClick={burnToken}>Burn</Button>
+							<Button variant='solidRadial'
+								m='0 auto'
+								size='lg'
+								minWidth='230px'
+								textTransform='uppercase'
+								color="white"
+								onClick={burnToken}>
+                Burn
+							</Button>
 						</Box>
 					</FormControl>
 				</Box>
