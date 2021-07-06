5e4bc162ebe568c1c7701452e70bb47fab30d195
hankxstudio <hankx@momane.com>
2021-07-06 22:50:08 +0800

    add basic responsive menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,71 @@
 import React from 'react'
-import { Flex } from '@chakra-ui/react'
+import { Box, Flex, Menu, MenuItem, IconButton, MenuButton, MenuList } from '@chakra-ui/react'
+import { HamburgerIcon } from '@chakra-ui/icons'
 import defaults from '../common/defaults'
 import { Link } from 'react-router-dom'
 import { Logotype } from './Logotype'
 import { WalletConnectionToggle } from './WalletConnectionToggle'
 
 export const Header = (props) => {
+	const pages = [
+		{
+			name: 'swap',
+			text: 'Swap',
+			link: '/',
+		},
+		{
+			name: 'liquidity',
+			text: 'Liquidity',
+			link: '/liquidity',
+		},
+		{
+			name: 'synths',
+			text: 'Synths',
+			link: '/synths',
+		},
+		{
+			name: 'redeem',
+			text: 'Redeem',
+			link: '/redeem',
+		},
+	]
 
 	return (
 		<Flex
+			style={{ justifyContent: 'space-between', alignItems: 'center' }}
 			minH={defaults.layout.header.minHeight}
 			{...props}>
-			<Flex w='33%'>
-				<Logotype margin='0 8px 0'/>
+			<Flex w={{ md: '33%', sm: '50%' }}>
+				<Logotype margin='0 8px 0' />
 			</Flex>
-			<Flex w='33%'
-				alignItems='center'
-				justifyContent='space-around'
-				textTransform='uppercase'
-			>
-				<Link to='/'>Swap</Link>
-				<Link to='/'>Liquidity</Link>
-				<Link to='/'>Synths</Link>
-				<Link to='/redeem'>Redeem</Link>
+			<Flex w='67%' display={{ base: 'none', md: 'flex' }}>
+				<Flex w='50%'
+					alignItems='center'
+					justifyContent='space-around'
+					textTransform='uppercase'
+				>
+					{pages.map(p => <Link key={p.name} to={p.link}>{p.text}</Link>)}
+				</Flex>
+				<Flex w='50%'
+					justifyContent='flex-end'
+					alignItems='center'
+				>
+					<WalletConnectionToggle />
+				</Flex>
 			</Flex>
-			<Flex w='33%'
-				justifyContent='flex-end'
-				alignItems='center'
-			>
-				<WalletConnectionToggle/>
-			</Flex>
+
+			<Menu>
+				<MenuButton display={{ base: 'flex', md: 'none' }}
+					as={IconButton}
+					aria-label='Options'
+					icon={<HamburgerIcon />}
+					variant='outline'
+				/>
+				<MenuList>
+					{pages.map(p => <MenuItem key={p.name}><Link to={p.link}>{p.text}</Link></MenuItem>)}
+					<WalletConnectionToggle />
+				</MenuList>
+			</Menu>
 		</Flex>
 	)
 }
