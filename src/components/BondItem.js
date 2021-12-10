cdf81f0be72440f6fb4e9846596c8885c0bdcce0
yellowgh0st <ZjAYxgG2kHyZbpuv@protonmail.com>
2021-12-10 08:15:50 +0100

    Create bondPrice hook

--- src/components/BondItem.js
+++ src/components/BondItem.js
@@ -9,6 +9,7 @@ import { CheckCircleIcon } from '@chakra-ui/icons'
 import { prettifyCurrency } from '../common/utils'
 import { bondInfo, bondPrice, bondPayoutToken, resolveERC20 } from '../common/ethereum'
 import defaults from '../common/defaults'
+import { useBondPrice } from '../hooks/useBondPrice'
 
 export const BondItem = (props) => {
 
@@ -21,10 +22,10 @@ export const BondItem = (props) => {
 
 	const wallet = useWallet()
 	const [accountBondInfo, setAccountBondInfo] = useState([])
-	const [bondItemPrice, setBondItemPrice] = useState(ethers.BigNumber.from(0))
 	const [bondItemPayoutToken, setBondItemPayoutToken] = useState(undefined)
 	const [bondItemToken0, setBondItemToken0] = useState(undefined)
 	const [bondItemToken1, setBondItemToken1] = useState(undefined)
+	const [bondItemPriceGQL] = useBondPrice()
 
 	useEffect(() => {
 		if (props.address && wallet.account) {
@@ -34,14 +35,6 @@ export const BondItem = (props) => {
 				.catch(err => console.log(err))
 		}
 	}, [props.address, wallet.account])
-
-	useEffect(() => {
-		if (props.address) {
-			bondPrice(props.address)
-				.then((bn) => setBondItemPrice(bn))
-				.catch(err => console.log(err))
-		}
-	}, [props.address])
 
 	useEffect(() => {
 		if (props.address) {
@@ -78,46 +71,44 @@ export const BondItem = (props) => {
 
 	return (
 		<>
-			{props.address	&& accountBondInfo && bondItemToken0 && bondItemPayoutToken &&
-			bondItemPrice &&
-				<Link to={`/bond/${props.address}`}>
+			<Link to={`/bond/${props.address}`}>
+				<Flex
+					width='100%'
+					alignItems='center'
+					justifyContent='space-between'
+					p='0 24px'
+					minH='60px'
+					cursor='pointer'
+					animation={ accountBondInfo?.payout?.gt(0) ? '2.3s ease-in-out infinite bgAnimation' : '' }
+					transition='all 0.3s ease 0s'
+					background='rgba(244, 155, 202, 0.08) none repeat scroll 0% 0%'
+					mb='16px'
+					borderRadius='16px'
+					border={ accountBondInfo?.payout?.gt(0) ? '1px solid #ffffff10' : '1px solid #ffffff08' }
+					_hover={{
+						cursor: 'pointer',
+						background: 'rgba(244, 155, 202, 0.2) none repeat scroll 0% 0%',
+						border: '1px solid #ffffff10',
+					}}
+				>
 					<Flex
-						width='100%'
-						alignItems='center'
-						justifyContent='space-between'
-						p='0 24px'
-						minH='60px'
-						cursor='pointer'
-						animation={ accountBondInfo?.payout?.gt(0) ? '2.3s ease-in-out infinite bgAnimation' : '' }
-						transition='all 0.3s ease 0s'
-						background='rgba(244, 155, 202, 0.08) none repeat scroll 0% 0%'
-						mb='16px'
-						borderRadius='16px'
-						border={ accountBondInfo?.payout?.gt(0) ? '1px solid #ffffff10' : '1px solid #ffffff08' }
-						_hover={{
-							cursor: 'pointer',
-							background: 'rgba(244, 155, 202, 0.2) none repeat scroll 0% 0%',
-							border: '1px solid #ffffff10',
-						}}
-					>
-						<Flex
-							fontWeight='bolder'>
-							<Image
-								width='23px'
-								height='23px'
-								borderRadius='50%'
-								mr='7px'
-								src={bondItemToken0?.logoURI}
-							/>
-							<Image
-								width='23px'
-								height='23px'
-								borderRadius='50%'
-								mr='10px'
-								src={bondItemToken1?.logoURI}
-							/>
-							{bondItemToken0?.symbol}{bondItemToken1 ? ` / ${bondItemToken1.symbol}` : ''}
-							{accountBondInfo?.payout?.gt(0) &&
+						fontWeight='bolder'>
+						<Image
+							width='23px'
+							height='23px'
+							borderRadius='50%'
+							mr='7px'
+							src={bondItemToken0?.logoURI}
+						/>
+						<Image
+							width='23px'
+							height='23px'
+							borderRadius='50%'
+							mr='10px'
+							src={bondItemToken1?.logoURI}
+						/>
+						{bondItemToken0?.symbol}{bondItemToken1 ? ` / ${bondItemToken1.symbol}` : ''}
+						{accountBondInfo?.payout?.gt(0) &&
 							<Tag
 								ml='10px'
 								colorScheme='green'>
@@ -125,30 +116,40 @@ export const BondItem = (props) => {
 									mr='5px'
 								/> BOUGHT
 							</Tag>
-							}
-						</Flex>
-						<Flex
-							flexDir='row'
-							alignContent='flex-end'
-							justifyContent='flex-end'
-							gridGap='0.7rem'
-						>
-							<Tag colorScheme='blue'>
-								{prettifyCurrency(ethers.utils.formatUnits(bondItemPrice, 18), 0, 4)}
-							</Tag>
-							<Tag colorScheme='blue'>
-								{bondItemPayoutToken?.symbol}
-							</Tag>
-							<Tag colorScheme='blue'>
-							100%
-							</Tag>
-							<Tag colorScheme='blue'>
-								{prettifyCurrency(999999999, 0, 4)}
-							</Tag>
-						</Flex>
+						}
 					</Flex>
-				</Link>
-			}
+					<Flex
+						flexDir='row'
+						alignContent='flex-end'
+						justifyContent='flex-end'
+						gridGap='0.7rem'
+					>
+						<Tag colorScheme='blue'>
+							{bondItemPriceGQL?.bondPriceChangedEvents?.[0].internalPrice &&
+								<>
+									{prettifyCurrency(
+										ethers.utils.formatUnits(
+											bondItemPriceGQL?.bondPriceChangedEvents?.[0].internalPrice,
+											18,
+										),
+										0,
+										4,
+									)}
+								</>
+							}
+						</Tag>
+						<Tag colorScheme='blue'>
+							{bondItemPayoutToken?.symbol}
+						</Tag>
+						<Tag colorScheme='blue'>
+							100%
+						</Tag>
+						<Tag colorScheme='blue'>
+							{prettifyCurrency(999999999, 0, 4)}
+						</Tag>
+					</Flex>
+				</Flex>
+			</Link>
 		</>
 	)
 }
