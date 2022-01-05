5d1463fb5078f9205c6015da94e5a7cbaf2836e8
yellowgh0st <ZjAYxgG2kHyZbpuv@protonmail.com>
2022-01-05 03:46:36 +0100

    Reduce padding

--- src/locations/bond.js
+++ src/locations/bond.js
@@ -391,7 +391,7 @@ const Bond = (props) => {
 				<Box
 					maxWidth={defaults.layout.container.md.width}
 					m='0 auto'
-					p={{ base: '5rem 1.1rem 6rem', md: '5rem 0 6rem' }}
+					p={{ base: '5rem 1.1rem 0', md: '5rem 0 0' }}
 					{...props}
 				>
 			 <Flex
