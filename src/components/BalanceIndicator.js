643abaeac88e9ea23609b7b536b6bcca3cefb292
yellowgh0st <ZjAYxgG2kHyZbpuv@protonmail.com>
2022-01-05 01:45:27 +0100

    Display balances in popover

--- src/components/BalanceIndicator.js
+++ src/components/BalanceIndicator.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { useQuery } from 'react-query'
 import { BigNumber, utils } from 'ethers'
-import { Button, Box, Image, Fade } from '@chakra-ui/react'
+import { Button, Box, Image, Fade, Popover, PopoverTrigger,
+	Portal, PopoverContent, PopoverCloseButton,
+	PopoverBody, Flex, Container } from '@chakra-ui/react'
 import defaults from '../common/defaults'
 import { useWallet } from 'use-wallet'
 import { useXvaderPrice } from '../hooks/useXvaderPrice'
@@ -11,7 +14,7 @@ import { prettifyNumber } from '../common/utils'
 export const BalanceIndicator = () => {
 
 	const wallet = useWallet()
-	const xvdrExchangeRate = useXvaderPrice(0)
+	const xvdrExchangeRate = useXvaderPrice()
 
 	const vaderBalance = useQuery(`${defaults.vader.address}_erc20Balanceof_${wallet?.account}`,
 		async () => {
@@ -41,65 +44,154 @@ export const BalanceIndicator = () => {
 		},
 	)
 
-	const totalBalance = () => {
+	const totalBalance = (total = true) => {
 		if (xvdrExchangeRate?.[0]?.global.value && xvaderBalance.data && vaderBalance.data) {
 			return utils.formatEther(
 				BigNumber.from(xvdrExchangeRate?.[0]?.global?.value)
 					.mul(xvaderBalance?.data)
 					.div(utils.parseUnits('1', 18))
-					.add(vaderBalance?.data))
+					.add(total ? vaderBalance?.data : 0))
 		}
+	}
+
+	const Item = (props) => {
+
+		Item.propTypes = {
+			name: PropTypes.string.isRequired,
+			token: PropTypes.object.isRequired,
+			value: PropTypes.any.isRequired,
+		}
+
+		return (
+			<Box>
+				<Box>
+					{props.name}
+				</Box>
+				<Flex>
+					<Container p='0'>
+						<Box>
+							<Flex
+								justifyContent='flex-start'
+								fontWeight='bolder'>
+								<Image
+									width='24px'
+									height='24px'
+									mr='5px'
+									src={props.token.logoURI}
+									alt={`${props.token.name} token`}
+								/>
+								{props.token.symbol}
+							</Flex>
+						</Box>
+					</Container>
+					<Container p='0'>
+						<Box
+							fontSize='1.2rem'
+							fontWeight='bold'
+							textAlign='right'
+						>
+							{props.value}
+						</Box>
+					</Container>
+				</Flex>
+			</Box>
+		)
 	}
 
 	return (
 		<>
 			{Number(totalBalance()) > 0 &&
 				<>
-					<Fade
-						unmountOnExit={true}
-						in={Number(totalBalance()) > 0}
+					<Popover
+						autoFocus={false}
+						preventOverflow={true}
+						closeOnEsc={true}
 					>
-						<Button
-							size='md'
-							fontSize={{ base: '0.65rem', sm: 'sm' }}
-							variant='outlineAlter'
-							display='flex'
-							flexDirection='row'
-							alignItems='center'
-							minW='84px'
-							transform={{
-								base: 'scale(0.86)',
-								sm: 'scale(1)',
-							}}
-						>
-							<Box
-								display='inline-flex'
-								alignItems='center'
-								justifyContent='center'
-								borderRadius='12px'
-								background='#000'
-								p='7px 11px'
+						<PopoverTrigger>
+							<Fade
+								unmountOnExit={true}
+								in={Number(totalBalance()) > 0}
 							>
-								<Box d='flex' alignItems='center'>
-									<Image
-										width='24px'
-										height='24px'
-										mr='5px'
-										src={defaults.vader.logoURI}
-										alt={`${defaults.vader.name} token`}
-									/>
+								<Button
+									size='md'
+									fontSize={{ base: '0.65rem', sm: 'sm' }}
+									variant='outlineAlter'
+									display='flex'
+									flexDirection='row'
+									alignItems='center'
+									minW='84px'
+									transform={{
+										base: 'scale(0.86)',
+										sm: 'scale(1)',
+									}}
+								>
 									<Box
-										as='h3'
-										m='0'
-										fontSize='1.02rem'
-										fontWeight='bold'
-										textTransform='capitalize'>
-										{prettifyNumber(totalBalance())}
+										display='inline-flex'
+										alignItems='center'
+										justifyContent='center'
+										borderRadius='12px'
+										background='#000'
+										p='7px 11px'
+									>
+										<Box d='flex' alignItems='center'>
+											<Image
+												width='24px'
+												height='24px'
+												mr='5px'
+												src={defaults.vader.logoURI}
+												alt={`${defaults.vader.name} token`}
+											/>
+											<Box
+												as='h3'
+												m='0'
+												fontSize='1.02rem'
+												fontWeight='bold'
+												textTransform='capitalize'>
+												{prettifyNumber(totalBalance(true), 0, 2)}
+											</Box>
+										</Box>
 									</Box>
-								</Box>
-							</Box>
-						</Button>
-					</Fade>
+								</Button>
+							</Fade>
+						</PopoverTrigger>
+						<Portal>
+							<PopoverContent>
+								<PopoverCloseButton
+									top='0.53rem'
+								/>
+								<PopoverBody
+									display='flex'
+									flexDir='column'
+									gridGap='0.9rem'
+									padding='1.6rem 1.6rem'
+								>
+									{Number(totalBalance(false)) > 0 &&
+										<Item
+											name={'Staking'}
+											token={defaults.vader}
+											value={prettifyNumber(totalBalance(false), 0, 2)}
+										/>
+									}
+
+									{xvaderBalance.data.gt(0) &&
+										<Item
+											name={'Balance'}
+											token={defaults.xvader}
+											value={prettifyNumber(utils.formatEther(xvaderBalance.data), 0, 2)}
+										/>
+									}
+
+									{vaderBalance.data.gt(0) &&
+										<Item
+											name={'Balance'}
+											token={defaults.vader}
+											value={prettifyNumber(utils.formatEther(vaderBalance.data), 0, 2)}
+										/>
+									}
+								</PopoverBody>
+							</PopoverContent>
+						</Portal>
+					</Popover>
 				</>
 			}
 		</>
