c7401ab41130bcdbe03eafd94067593c9e228e3d
dimorphic <dimorphic@users.noreply.github.com>
2022-01-06 23:32:20 +0200

    [message] Refactor the 🍞 toaster machine - reduce toaster default config boilerplate & code lines #DRY

--- src/messages.js
+++ src/messages.js
@@ -1,248 +1,180 @@
 import defaults from './common/defaults'
 
-const approved = {
+// the 🍞 Toaster 🤖 #mhm #tasty
+const createToast = ({ title, description, status, ...rest }) => ({
+	title,
+	description,
+	status,
+
+	duration: defaults.toast.duration,
+	isClosable: defaults.toast.closable,
+	position: defaults.toast.position,
+
+	...rest,
+})
+
+const approved = createToast({
 	title: 'Token succesfully approved',
 	description: 'You have approved the token for spending.',
 	status: 'success',
-	duration: defaults.toast.duration,
-	isClosable: defaults.toast.closable,
-	position: defaults.toast.position,
-}
+})
 
-const notBurnEligible = {
+const notBurnEligible = createToast({
 	title: 'Account not eligible',
 	description: 'Your account is not eligible to burn this token.',
 	status: 'error',
-	duration: defaults.toast.duration,
-	isClosable: defaults.toast.closable,
-	position: defaults.toast.position,
-}
+})
 
-const featureNotAvailable = {
+const featureNotAvailable = createToast({
 	title: 'This feature is not available',
 	description: 'This option will be implemented in the near future.',
 	status: 'error',
-	duration: defaults.toast.duration,
-	isClosable: defaults.toast.closable,
-	position: defaults.toast.position,
-}
+})
 
-const tokenNotAvailableToselect = {
+const tokenNotAvailableToselect = createToast({
 	title: 'This token cannot be selected',
 	description: 'This option will be implemented in the near future.',
 	status: 'error',
-	duration: defaults.toast.duration,
-	isClosable: defaults.toast.closable,
-	position: defaults.toast.position,
-}
+})
 
-const connected = {
+const connected = createToast({
 	title: 'Wallet connected',
 	description: 'Your wallet has been connected.',
 	status: 'success',
-	duration: defaults.toast.duration,
-	isClosable: defaults.toast.closable,
-	position: defaults.toast.position,
-}
+})
 
-const insufficientBalance = {
+const insufficientBalance = createToast({
 	title: 'Insufficient balance',
 	description: 'Your account balance is insufficient.',
 	status: 'error',
-	duration: defaults.toast.duration,
-	isClosable: defaults.toast.closable,
-	position: defaults.toast.position,
-}
+})
 
-const rejected = {
+const rejected = createToast({
 	title: 'Transaction rejected',
 	description: 'You have rejected the transaction. Please try again.',
 	status: 'success',
-	duration: defaults.toast.duration,
-	isClosable: defaults.toast.closable,
-	position: defaults.toast.position,
-}
+})
 
-const failed = {
+const failed = createToast({
 	title: 'Transaction failed',
 	description: 'Transaction failed. Please try again',
 	status: 'error',
-	duration: defaults.toast.duration,
-	isClosable: defaults.toast.closable,
-	position: defaults.toast.position,
-}
+})
 
-const exception = {
+const exception = createToast({
 	title: 'Transaction error',
 	description: 'This transaction will be reverted. Please try again.',
 	status: 'error',
-	duration: defaults.toast.duration,
-	isClosable: defaults.toast.closable,
-	position: defaults.toast.position,
-}
+})
 
 
-const walletNotConnected = {
+const walletNotConnected = createToast({
 	title: 'Wallet not connected',
 	description: 'Please connect a wallet.',
 	status: 'warning',
-	duration: defaults.toast.duration,
-	isClosable: defaults.toast.closable,
-	position: defaults.toast.position,
-}
+})
 
-const vaderconverted = {
+const vaderconverted = createToast({
 	title: 'VADER Token burnt for USDV',
 	description: 'Your VADER tokens were successfully burnt for USDV.',
 	status: 'success',
-	duration: defaults.toast.duration,
-	isClosable: defaults.toast.closable,
-	position: defaults.toast.position,
-}
+})
 
-const vaderclaimed = {
+const vaderclaimed = createToast({
 	title: 'VADER succesfully claimed',
 	description: 'Your VADER tokens were successfully claimed.',
 	status: 'success',
-	duration: defaults.toast.duration,
-	isClosable: defaults.toast.closable,
-	position: defaults.toast.position,
-}
+})
 
-const usdvredeemed = {
+const usdvredeemed = createToast({
 	title: 'USDV burnt for VADER Token',
 	description: 'Your USDV tokens were successfully burnt for VADER.',
 	status: 'success',
-	duration: defaults.toast.duration,
-	isClosable: defaults.toast.closable,
-	position: defaults.toast.position,
-}
+})
 
-const vethupgraded = {
+const vethupgraded = createToast({
 	title: 'VETH burnt for VADER Token',
 	description: 'Your VETH tokens were succesfully burnt for VADER.',
 	status: 'success',
-	duration: defaults.toast.duration,
-	isClosable: defaults.toast.closable,
-	position: defaults.toast.position,
-}
+})
 
-const noAmount = {
+const noAmount = createToast({
 	title: 'No amount specified',
 	description: 'Please specify amount.',
 	status: 'warning',
-	duration: defaults.toast.duration,
-	isClosable: defaults.toast.closable,
-	position: defaults.toast.position,
-}
+})
 
-const noToken0 = {
+const noToken0 = createToast({
 	title: 'No token selected',
 	description: 'Please select a token.',
 	status: 'warning',
-	duration: defaults.toast.duration,
-	isClosable: defaults.toast.closable,
-	position: defaults.toast.position,
-}
+})
 
-const nothingtoclaim = {
+const nothingtoclaim = createToast({
 	title: 'Everything already claimed',
 	description: 'There\'s no claim left. Everything was already claimed.',
 	status: 'success',
-	duration: defaults.toast.duration,
-	isClosable: defaults.toast.closable,
-	position: defaults.toast.position,
-}
+})
 
-const nomorethaneligible = {
+const nomorethaneligible = createToast({
 	title: 'Cannot burn more than eligible',
 	description: 'Please specify burn amount that is up to eligible amount.',
 	status: 'error',
-	duration: defaults.toast.duration,
-	isClosable: defaults.toast.closable,
-	position: defaults.toast.position,
-}
+})
 
-const tokenValueTooSmall = {
+const tokenValueTooSmall = createToast({
 	title: 'Token amount is too small',
 	description: 'Please input a larger token amount.',
 	status: 'warning',
-	duration: defaults.toast.duration,
-	isClosable: defaults.toast.closable,
-	position: defaults.toast.position,
-}
+})
 
-const positionOpened = {
+const positionOpened = createToast({
 	title: 'Liquidity succesfully provided',
 	description: 'You\'ve opened liquidity position.',
 	status: 'success',
-	duration: defaults.toast.duration,
-	isClosable: defaults.toast.closable,
-	position: defaults.toast.position,
-}
+})
 
-const staked = {
+const staked = createToast({
 	title: 'VADER tokens staked',
 	description: 'Your VADER tokens have been staked succesfully.',
 	status: 'success',
-	duration: defaults.toast.duration,
-	isClosable: defaults.toast.closable,
-	position: defaults.toast.position,
-}
+})
 
-const bondConcluded = {
+const bondConcluded = createToast({
 	title: 'Bond purchase succesfull',
 	description: 'The bond purchase has been concluded.',
 	status: 'success',
-	duration: defaults.toast.duration,
-	isClosable: defaults.toast.closable,
-	position: defaults.toast.position,
-}
+})
 
-const bondPurchaseValueExceeds = {
+const bondPurchaseValueExceeds = createToast({
 	title: 'Purchase exceeds transaction cap',
 	description: 'Your can\'t purchase more than is transaction cap.',
 	status: 'error',
-	duration: defaults.toast.duration,
-	isClosable: defaults.toast.closable,
-	position: defaults.toast.position,
-}
+})
 
-const bondSoldOut = {
+const bondSoldOut = createToast({
 	title: 'Bond sale has been sold out',
 	description: 'This sale has been sold out and cannot be purchased.',
 	status: 'warning',
-	duration: defaults.toast.duration,
-	isClosable: defaults.toast.closable,
-	position: defaults.toast.position,
-}
+})
 
-const bondAmountTooSmall = {
+const bondAmountTooSmall = createToast({
 	title: 'Amount to buy bond with is too small',
 	description: 'Please input a larger amount.',
 	status: 'error',
-	duration: defaults.toast.duration,
-	isClosable: defaults.toast.closable,
-	position: defaults.toast.position,
-}
+})
 
-const unstaked = {
+const unstaked = createToast({
 	title: 'xVADER tokens unstaked',
 	description: 'Your xVADER tokens have been unstaked succesfully.',
 	status: 'success',
-	duration: defaults.toast.duration,
-	isClosable: defaults.toast.closable,
-	position: defaults.toast.position,
-}
+})
 
-const copiedContractAddress = {
+const copiedContractAddress = createToast({
 	title: 'Contract Address',
 	description: 'Address copied! Go pasta!',
 	status: 'success',
-	duration: defaults.toast.duration,
-	isClosable: defaults.toast.closable,
-	position: defaults.toast.position,
-}
+})
 
 export {
 	approved, connected, failed, rejected, insufficientBalance,
