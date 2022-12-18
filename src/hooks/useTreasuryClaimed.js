65d921368814c2068a0917edfc9a0a4fc3c3d753
yell0wghost <augustin.be@pm.me>
2022-12-18 16:42:28 +0100

    Check for treasury claimed

---
+++ src/hooks/useTreasuryClaimed.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+import { useQuery as useApolloQuery, gql } from '@apollo/client'
+import { useQuery } from 'react-query'
+import { getTreasuryClaimed } from '../common/ethereum'
+import defaults from '../common/defaults'
+import { useWallet } from 'use-wallet'
+import { BigNumber } from 'ethers'
+
+export const useTreasuryClaimed = (
+	account = '',
+	amount = BigNumber.from(0),
+	staleTime = defaults.api.staleTime) => {
+
+	const wallet = useWallet()
+
+	const claimed = useQuery(`treasuryClaimed_${account ? account : wallet?.account}`, async () => {
+		return await getTreasuryClaimed(account ? account : wallet.account)
+	}, {
+		staleTime: staleTime,
+		enabled: !!account || !!wallet.account,
+	},
+	)
+
+	return claimed
+}
--- src/locations/redeem.js
+++ src/locations/redeem.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import {
 	Box,
-	Badge,
 	Flex,
 	Text,
 	Button,
@@ -16,11 +15,13 @@ import { CheckCircleIcon } from '@chakra-ui/icons'
 import { BsFillXCircleFill, BsQuestionCircle } from 'react-icons/bs'
 import { useWallet } from 'use-wallet'
 import { walletNotConnected } from '../messages'
+import { useTreasuryClaimed } from '../hooks/useTreasuryClaimed'
 
 const Burn = (props) => {
 
 	const wallet = useWallet()
 	const toast = useToast()
+	const { data: claimed } = useTreasuryClaimed()
 	const [address, setAddress] = useState('')
 	const [eligible, setEligible] = useState(false)
 	const [working, setWorking] = useState(false)
@@ -50,7 +51,7 @@ const Burn = (props) => {
 					w='100%'
 					maxW='49ch'
 					m='0 auto'r
-					minH={{ base: 'auto', md: '588.617px' }}
+					minH={{ base: '588.617px' }}
 					p={{ base: '2rem 0.9rem', md: '2rem 2.6rem' }}
 					layerStyle='colorful'
 					flexDir='column'
@@ -75,94 +76,103 @@ const Burn = (props) => {
 						</Text>
 					</Flex>
 					<Flex
-						flexDir='column'
-						gap='1rem'
-					>
-						<Flex
-							flexDir='row'
-							gap='.7rem'
-						>
-							{!wallet.account &&
-								<>
-									<Icon
-										as={BsQuestionCircle}
-										boxSize='1.33rem'
-										marginTop='3px'/>
-									<Box as='span'>Connect your wallet account or enter adddres below to check eligibility.</Box>
-								</>
-							}
-							{wallet.account &&
-								<>
-									{eligible &&
-										<>
-											<Icon
-												color='#6fc2ff'
-												as={CheckCircleIcon}
-												{...iconSize}/>
-											<Box as='span'>Your wallet account is eligible to claim proportional amount of the protocol treasury.</Box>
-										</>
-									}
-									{!eligible &&
-										<>
-											<Icon
-												as={BsFillXCircleFill}
-												color='#ff9292'
-												{...iconSize}/>
-											<Box>
-												Your wallet account is not eligible to claim.
-											</Box>
-										</>
-									}
-								</>
-							}
-						</Flex>
-					</Flex>
-
-					<Flex
-						m='0 0 1.2rem'
-						flexDir='column'
+						flexDirection='column'
+						gap='2.7rem'
 					>
 						<Flex
 							flexDir='column'
+							gap='1rem'
 						>
-							{wallet.account && !eligible &&
-								<>
-									However, you can still trigger claim for other one.
-								</>
-							}
-							<Text
-								as='h4'
-								fontWeight='bolder'>
-									Claim for account
-							</Text>
 							<Flex
-								mt='.3rem'
-								justifyContent='flex-start'
 								flexDir='row'
+								gap='.7rem'
 							>
-								<InputGroup
-									size='sm'
+								{!wallet.account &&
+									<>
+										<Icon
+											as={BsQuestionCircle}
+											boxSize='1.33rem'
+											marginTop='3px'/>
+										<Box as='span'>Connect your wallet account or enter adddres below to check eligibility.</Box>
+									</>
+								}
+								{wallet.account &&
+									<>
+										{claimed &&
+											<>
+												<Icon
+													color='#6fc2ff'
+													as={CheckCircleIcon}
+													{...iconSize}/>
+												<Box as='span'>Treasury share has already been claimed.</Box>
+											</>
+										}
+										{eligible && !claimed &&
+											<>
+												<Icon
+													color='#6fc2ff'
+													as={CheckCircleIcon}
+													{...iconSize}/>
+												<Box as='span'>You can claim a portion of the protocol treasury with your wallet account.</Box>
+											</>
+										}
+										{!eligible &&
+											<>
+												<Icon
+													as={BsFillXCircleFill}
+													color='#ff9292'
+													{...iconSize}/>
+												<Box>
+													There are no claimable shares for your wallet account.<br/>
+													You can still make a claim for another one, though.
+												</Box>
+											</>
+										}
+									</>
+								}
+							</Flex>
+						</Flex>
+						<Flex
+							m='0 0 1.2rem'
+							flexDir='column'
+						>
+							<Flex
+								flexDir='column'
+							>
+								<Text
+									as='h4'
+									fontWeight='bolder'>
+									Claim for account
+								</Text>
+								<Flex
+									mt='.3rem'
+									justifyContent='flex-start'
+									flexDir='row'
 								>
-									<Input
-										variant='outline'
-										placeholder='Address'
-										value={address}
-										onChange={(e) => {
-											const regex = new RegExp('^[a-fA-F0-9]+$')
-											const val = String(e.target.value)
-											if (val.length > 42 ||
+									<InputGroup
+										size='sm'
+									>
+										<Input
+											variant='outline'
+											placeholder='Address'
+											value={address}
+											onChange={(e) => {
+												const regex = new RegExp('^[a-fA-F0-9]+$')
+												const val = String(e.target.value)
+												if (val.length > 42 ||
 												val.length === 1 && val.slice(0, 1) !== '0' ||
 												val.length === 2 && val.slice(0, 2) !== '0x' ||
 												val.length > 2 && regex.test(val.substring(2)) === false
-											) {
-												setAddress(prev => prev)
-											}
-											else {
-												setAddress(String(e.target.value))
-											}
-										}}
-									/>
-								</InputGroup>
+												) {
+													setAddress(prev => prev)
+												}
+												else {
+													setAddress(String(e.target.value))
+												}
+											}}
+										/>
+									</InputGroup>
+								</Flex>
 							</Flex>
 						</Flex>
 					</Flex>
